89f6763d9786d452aa736c279027a14921230ca6
Otavio-PC <otavio.aspc@gmail.com>
2024-02-21 15:17:05 -0300

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Button from './Components/Button'
-import Input from './Components/CustomInput'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Button from "./Components/Button";
+import Input from "./Components/CustomInput";
+import Footer from "./Components/Footer";
 
 function App() {
-    return (
-        <div className="App">
-          <main>
-            <Router>
-              <Button/>
-              <Input/>
-            </Router>
-          </main>
-        </div>
-    );
+  return (
+    <div className="App">
+      <main>
+        <Router>
+          <Button />
+          <Input />
+          <Footer />
+        </Router>
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,1 +1,37 @@
-export {}
+import { useLocation } from "react-router-dom";
+import styles from "../Styles/Footer.module.css";
+
+const Footer = () => {
+  const location = useLocation();
+  const isPublicPage = location.pathname === "/login/";
+
+  if (!isPublicPage) {
+    return null;
+  }
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.topics}>
+        <p className={styles.description}>
+          <strong>Objetivo:</strong> Fornecer uma plataforma capaz de gerir as
+          eleições vinculadas a Defensoria Pública de maneira organizada e
+          prática.
+        </p>
+        <p className={styles.copyright}>
+          &copy; Desenvolvido pela Gerência de Tecnologia da Informação da
+          Defensoria Pública do Estado de Santa Catarina. Todos os direitos
+          reservados {new Date().getFullYear()}.
+        </p>
+        <p className={styles.description}>
+          <strong>Endereço:</strong> Av. Rio Branco, nº 919, Ed. Centro
+          Executivo Rio Branco, Centro, Florianópolis/SC, 88015-205
+          <br />
+          <strong>Contato:</strong> (48) 3665-6370 / (48) 3665-6589 / (48)
+          3665-6654
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
