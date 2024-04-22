3550329101ad67f7a3ef37e039e367518323e806
Guilherme Pereira <guilhermepereira@defensoria.sc.gov.br>
2024-04-22 16:46:21 -0300

    Feat: adjust votePage

--- src/Pages/VotePagePdf.tsx
+++ src/Pages/VotePagePdf.tsx
@@ -1,45 +1,33 @@
-import React, { useEffect, useState } from "react";
-import styles from "../Styles/VotePagePdf.module.css";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import { useDispatch, useSelector } from "react-redux";
-import Snackbar from "../Components/Snackbar";
+import React from "react";
+import { BiSolidDownload } from "react-icons/bi";
+import services from "../Services/services";
 import { Button } from "@mui/material";
-import { MdDownload } from "react-icons/md";
-import { fetchFileContent } from "../Services/Slices/fileContentSlice";
 
-const VotePagePdf: React.FC = () => {
-  const { data, loading, error } = useSelector(
-    (state: any) => state.fileContentSlice
-  );
-  const [isDispatched, setIsDispatched] = useState<boolean>(false);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch<any>(fetchFileContent());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (isDispatched) {
-      const file_name = "DIÁRIO OFICIAL Modelo.docx";
-      // const file = await services.downloadFiles(file_name);
-      const a = document.createElement("a");
-      a.href = data?.data?.url;
-      a.download = "template.pdf";
-      a.click();
+export const VotePagePdf = () => {
+  const handleDownload = async () => {
+    try {
+      const response = await services.downloadHashReportPDF();
+      downloadPDF(response, "hash_report.pdf");
+    } catch (error) {
+      console.error("Error downloading PDF:", error);
     }
-  }, [data, isDispatched]);
+  };
+
+  const downloadPDF = (pdfData: Blob, filename: string) => {
+    const url = URL.createObjectURL(pdfData);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
 
   return (
-    <div className={styles.container}>
-      {error && isDispatched && (
-        <Snackbar type="fileContentError" setShowSnackbar={setIsDispatched} />
-      )}
-      <h2 className={styles.title} style={{ color: "initial" }}>
-        PDF Vote Page
-      </h2>
-      <Button className={styles.button}>
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <Button onClick={handleDownload}>
         Comprovante
-        <MdDownload size={32} />
+        <BiSolidDownload size={24} />
       </Button>
     </div>
   );
