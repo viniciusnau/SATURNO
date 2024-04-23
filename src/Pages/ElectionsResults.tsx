955758443ae4aa00bd9ac2516192f7b509d339ef
Vinicius Nau <viniciuscoelhonau975@gmail.com>
2024-04-23 14:20:26 -0300

    fix: NaN

--- src/Pages/ElectionsResults.tsx
+++ src/Pages/ElectionsResults.tsx
@@ -27,6 +27,9 @@ const ElectionsResults = () => {
 
   const formatDate = (dateString: string | number | Date) => {
     const date = new Date(dateString);
+    if (isNaN(date.getTime())) {
+      return '';
+  }
     const day = date.getDate().toString().padStart(2, '0');
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const year = date.getFullYear();
