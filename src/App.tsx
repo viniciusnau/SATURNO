a0865ae49e0bc2e71fe3056c40e75c041dcb7bcf
Otavio-PC <otavio.aspc@gmail.com>
2024-05-16 10:46:58 -0300

    build: Setting a cookies clear component for avoid any cookie issue

--- src/App.tsx
+++ src/App.tsx
@@ -14,80 +14,83 @@ import ProtectedRoute from "./Auth/ProtectedRoute";
 import { Register } from "./Pages/Register";
 import { VoteReport } from "./Pages/VoteReport";
 import ApproveRegister from "./Pages/ApproveRegister";
+import { ClearCookiesProvider } from "./Components/ClearCookiesProvider";
 
 function App() {
   return (
     <div className="App">
       <main>
         <Router>
-          <Header />
-          <Routes>
-            <Route
-              path="/saturno/vote/"
-              element={
-                <ProtectedRoute Component={VotePage} path="/saturno/vote" />
-              }
-            />
-            <Route
-              path="/saturno/vote-pdf/"
-              element={
-                <ProtectedRoute
-                  Component={VotePagePdf}
-                  path="/saturno/vote-pdf/"
-                />
-              }
-            />
-            <Route path="/saturno/login/" element={<Login />} />
-            <Route
-              path="/saturno/password-reset/"
-              element={<ResetPassword />}
-            />
-            <Route
-              path="/saturno/confirm-hash/"
-              element={
-                <ProtectedRoute
-                  Component={HashValidation}
-                  path="/saturno/confirm-hash/"
-                />
-              }
-            />
-            <Route
-              path="/saturno/elections-results/"
-              element={
-                <ProtectedRoute
-                  Component={ElectionsResults}
-                  path="/saturno/elections-results/"
-                  accessRole={["electoral commission", "admin"]}
-                />
-              }
-            />
-            <Route
-              path="/saturno/vote-report/"
-              element={
-                <ProtectedRoute
-                  Component={VoteReport}
-                  path="/saturno/vote-report/"
-                  accessRole={["electoral commission", "admin"]}
-                />
-              }
-            />
-            <Route
-              path="/saturno/approve-register/"
-              element={
-                <ProtectedRoute
-                  Component={ApproveRegister}
-                  path="/saturno/approve-register/"
-                  accessRole={["admin"]}
-                />
-              }
-            />
-            <Route
-              path="/saturno/callback/:googleToken"
-              element={<Callback />}
-            />
-            <Route path="/saturno/register/" element={<Register />} />
-          </Routes>
-          <Footer />
+          <ClearCookiesProvider>
+            <Header />
+            <Routes>
+              <Route
+                path="/saturno/vote/"
+                element={
+                  <ProtectedRoute Component={VotePage} path="/saturno/vote" />
+                }
+              />
+              <Route
+                path="/saturno/vote-pdf/"
+                element={
+                  <ProtectedRoute
+                    Component={VotePagePdf}
+                    path="/saturno/vote-pdf/"
+                  />
+                }
+              />
+              <Route path="/saturno/login/" element={<Login />} />
+              <Route
+                path="/saturno/password-reset/"
+                element={<ResetPassword />}
+              />
+              <Route
+                path="/saturno/confirm-hash/"
+                element={
+                  <ProtectedRoute
+                    Component={HashValidation}
+                    path="/saturno/confirm-hash/"
+                  />
+                }
+              />
+              <Route
+                path="/saturno/elections-results/"
+                element={
+                  <ProtectedRoute
+                    Component={ElectionsResults}
+                    path="/saturno/elections-results/"
+                    accessRole={["electoral commission", "admin"]}
+                  />
+                }
+              />
+              <Route
+                path="/saturno/vote-report/"
+                element={
+                  <ProtectedRoute
+                    Component={VoteReport}
+                    path="/saturno/vote-report/"
+                    accessRole={["electoral commission", "admin"]}
+                  />
+                }
+              />
+              <Route
+                path="/saturno/approve-register/"
+                element={
+                  <ProtectedRoute
+                    Component={ApproveRegister}
+                    path="/saturno/approve-register/"
+                    accessRole={["admin"]}
+                  />
+                }
+              />
+              <Route
+                path="/saturno/callback/:googleToken"
+                element={<Callback />}
+              />
+              <Route path="/saturno/register/" element={<Register />} />
+            </Routes>
+            <Footer />
+          </ClearCookiesProvider>
         </Router>
       </main>
     </div>
